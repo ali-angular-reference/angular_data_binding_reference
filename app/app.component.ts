5b1f06f6987c2fed90eef2fd7d0fcd633ebd290e
Ali Harris <aliharriss1995@gmail.com>
2020-07-19 23:40:24 +1200

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Server } from 'selenium-webdriver/safari';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type:'server',name: 'testserver',content: 'a test'}]; 
+}

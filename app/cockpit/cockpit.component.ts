8d16dfcfd998a35364ed78a4d7d7a2e0068d3657
Ali Harris <aliharriss1995@gmail.com>
2020-08-04 23:23:18 +1200

    finished local referece changes

--- app/cockpit/cockpit.component.ts
+++ app/cockpit/cockpit.component.ts
@@ -10,7 +10,6 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   newServerContent = '';
-  newServerName = '';
   constructor() { }
 
   ngOnInit(): void {
@@ -18,11 +17,13 @@ export class CockpitComponent implements OnInit {
 
   onAddServer(serverNameInput){
     // these vals are binded to the dom
-    console.log(serverNameInput)
-    this.serverCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent})
+    this.serverCreated.emit({
+      serverName:serverNameInput,
+      serverContent:this.newServerContent})
   }
 
-  onAddBlueprint(){
-    this.blueprintCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent})
+  onAddBlueprint(serverNameInput){
+    this.blueprintCreated.emit({serverName:serverNameInput,
+      serverContent:this.newServerContent})
   }
 }
